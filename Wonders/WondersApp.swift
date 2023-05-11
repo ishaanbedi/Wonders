f925ff0c68817085bce121b208c6a2edaf2caf7e
Ishaan Bedi <bediishaan6@gmail.com>
2023-05-11 12:50:49 +0530

    Initial Commit

---
+++ Wonders/WondersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WondersApp.swift
+//  Wonders
+//
+//  Created by Ishaan Bedi on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WondersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
